06fc8296b4e9dc26a7d57675c956e8340cf12602
9064_linux (Student) <s_9064_linux@pedagogique.lan>
2017-11-07 12:14:08 +0100

    add exercices01 folder

---
+++ exercice01/H-w/src/app/hw/hw.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-hw',
+  templateUrl: './hw.component.html',
+  styleUrls: ['./hw.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HwComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
